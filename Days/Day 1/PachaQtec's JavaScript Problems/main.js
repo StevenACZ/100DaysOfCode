8fc80f562e53feb7a8b7b9a75be6fafc0f8051bb
Steven_coailaZ <scoailazaa@gmail.com>
2020-02-15 22:21:15 -0500

    Problem-2

--- Days/Day 1/PachaQtec's JavaScript Problems/main.js
+++ Days/Day 1/PachaQtec's JavaScript Problems/main.js
@@ -3,6 +3,19 @@
 // un nombre, apellido y edad y los retorne en un string concatenado 
 // “Hola mi nombre es sebastiányabiku y mi edad 33”
 // --------------------------------------------------------------------------
-
+/*
 const hi = (name, lastName, age) => `Hello my name is ${name} ${lastName} and I'm ${age} years old.`;
 console.log(hi('Steven', 'Zaa', 18));
+*/
+
+// (2) ----------------------------------------------------------------------
+// Cree una función que tome números y devuelva la suma de sus cubos.
+// sumOfCubes(1, 5, 9) ➞ 855
+// Since 1^3 + 5^3 + 9^3 = 1 + 125 + 729 = 855
+// --------------------------------------------------------------------------
+
+const sumCubos = (a, b, c) => {
+  return a**3 + b**3 + c**3; 
+};
+console.log('Suma de cubos: ' + sumCubos(1, 5, 9));
+
