b9c6da0ca8a4d03c9c224f4e1561b333e73c30ed
Steven_coailaZ <scoailazaa@gmail.com>
2020-02-16 16:27:26 -0500

    Problem-16

--- Days/Day 2/PachaQtec's JavaScript Problems/main.js
+++ Days/Day 2/PachaQtec's JavaScript Problems/main.js
@@ -225,7 +225,7 @@ console.log(squaresSum(3));
 
 // multiplyByLength([2, 3, 1, 0]) ➞ [8, 12, 4, 0]
 // --------------------------------------------------------------------------
-
+/*
 const multiplyByLength = (arr) => {
   let newArr = [];
   arr.forEach(e => {
@@ -234,3 +234,20 @@ const multiplyByLength = (arr) => {
   return newArr;
 }
 console.log(multiplyByLength([2, 3, 1, 0]));
+*/
+
+// (16) ----------------------------------------------------------------------
+// Cree una función que tome un número como argumento y devuelva una matriz 
+// de números contando desde este número a cero.
+
+// countdown(5) ➞ [5, 4, 3, 2, 1, 0]
+// --------------------------------------------------------------------------
+
+const countdown = (n) => {
+  let arr = [];
+  for (var i = 0; i <= n; i++) {
+    arr.unshift(i);
+  }
+  return arr;
+}
+console.log(countdown(5));
