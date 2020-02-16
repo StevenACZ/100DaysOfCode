b9400d74a11ccf9c5f7a261fd207d014c3a2b54d
Steven_coailaZ <scoailazaa@gmail.com>
2020-02-16 16:34:16 -0500

    Problem-17

--- Days/Day 2/PachaQtec's JavaScript Problems/main.js
+++ Days/Day 2/PachaQtec's JavaScript Problems/main.js
@@ -242,7 +242,7 @@ console.log(multiplyByLength([2, 3, 1, 0]));
 
 // countdown(5) ➞ [5, 4, 3, 2, 1, 0]
 // --------------------------------------------------------------------------
-
+/*
 const countdown = (n) => {
   let arr = [];
   for (var i = 0; i <= n; i++) {
@@ -251,3 +251,19 @@ const countdown = (n) => {
   return arr;
 }
 console.log(countdown(5));
+*/
+
+// (17) ----------------------------------------------------------------------
+// Cree una función que tome una matriz y devuelva la diferencia entre los 
+// números más grandes y más pequeños.
+
+// diffMaxMin([10, 4, 1, 4, -10, -50, 32, 21]) ➞ 82
+// Smallest number is -50, biggest is 32.
+// --------------------------------------------------------------------------
+
+const diffMaxMin = (arr) => {
+  let max = Math.max(...arr);
+  let min = Math.min(...arr);
+  return `Smallest number is ${min}, biggest is ${max}, the difference is ${max - min}.`;
+}
+console.log(diffMaxMin([10, 4, 1, 4, -10, -50, 32, 21]));
